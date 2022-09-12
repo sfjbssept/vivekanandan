055d1250ded0794b47de01e3cc2802344c348f6d
sfjbssept <sfjbssept@gmail.com>
2022-09-12 04:37:00 +0000

    spring initial commit

---
+++ ProductsmanagementSystem/src/main/java/com/product/entity/Product.java
@@ -0,0 +1,73 @@
+package com.product.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String productBrand;
+	private String productName;
+	private String productcategory;
+	private int productPrice;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductBrand() {
+		return productBrand;
+	}
+
+	public void setProductBrand(String productBrand) {
+		this.productBrand = productBrand;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductcategory() {
+		return productcategory;
+	}
+
+	public void setProductcategory(String productcategory) {
+		this.productcategory = productcategory;
+	}
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public Product(int id, String productBrand, String productName, String productcategory, int productPrice) {
+		super();
+		this.id = id;
+		this.productBrand = productBrand;
+		this.productName = productName;
+		this.productcategory = productcategory;
+		this.productPrice = productPrice;
+	}
+
+	public Product() {
+		super();
+	}
+
+}
---
+++ ProductsmanagementSystem/src/main/java/com/product/services/IProductService.java
@@ -0,0 +1,5 @@
+package com.product.services;
+
+public class IProductService {
+
+}
